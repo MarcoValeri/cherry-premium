809833049154a818ff5b853f831f876d3d354865
Marco Valeri <marco.valeri@cherrythinking.com>
2023-11-09 14:57:12 +0000

    Add theme support

--- functions.php
+++ functions.php
@@ -1,7 +1,12 @@
 <?php
 /**
- * Cherry Premium Theme
- * 
- * @package Cherry Premium
- * @since Cherry Premium 1.0
- */
+* Cherry Premium Theme
+* 
+* @package Cherry Premium
+* @since Cherry Premium 1.0
+*/
+
+// Add theme support
+add_theme_support('custom-logo');
+add_theme_support('menus');
+add_theme_support('widgets');
